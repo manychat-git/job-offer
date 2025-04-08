f17ef9416df1837ac3d23220ea03e2db134fd5e2
ManyChat <manychat@gmail.com>
2025-04-08 09:39:30 +0900

    Add word-break for candidate name in WelcomeMessage

--- src/components/pdf/molecules/WelcomeMessage.jsx
+++ src/components/pdf/molecules/WelcomeMessage.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const WelcomeMessage = ({ name }) => (
   <div className="w-96 px-5 py-4 bg-white rounded-[32px] flex flex-col justify-start items-start gap-1">
-    <div className="self-stretch justify-start text-black text-lg font-normal font-['Rooftop'] leading-tight">
-      Hi there, {name}! 👋 <br/>
+    <div className="self-stretch justify-start text-black text-lg font-normal font-['Rooftop'] leading-tight break-words">
+      Hi there, <span className="break-words overflow-wrap-anywhere">{name}</span>! 👋 <br/>
       We're absolutely overjoyed to extend <br/>
       you the following job offer!
     </div>
